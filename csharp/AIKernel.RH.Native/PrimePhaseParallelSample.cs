63b2a254830f79475fdac05067a209167d39046b
TKY S <tky4@hotmail.com>
2026-06-01 23:16:45 +0900

    Add chunked batched parallel sample

--- csharp/AIKernel.RH.Native/PrimePhaseParallelSample.cs
+++ csharp/AIKernel.RH.Native/PrimePhaseParallelSample.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace AIKernel.RH.Native;
 
@@ -21,5 +22,45 @@ public static class PrimePhaseParallelSample
             .ToArray();
 
         return PrimePhaseNative.GetInterferenceEnergyBatch(inputs);
+    }
+
+    public static ulong[] RunChunkedBatchedParallel(
+        ulong count,
+        int chunkSize = 10_000,
+        int? maxDegreeOfParallelism = null)
+    {
+        if (chunkSize <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(chunkSize));
+        }
+
+        var length = checked((int)count);
+        var inputs = Enumerable.Range(0, length)
+            .Select(i => (ulong)i)
+            .ToArray();
+        var outputs = new ulong[length];
+        var numberOfChunks = (length + chunkSize - 1) / chunkSize;
+        var options = new ParallelOptions();
+
+        if (maxDegreeOfParallelism is { } degree)
+        {
+            options.MaxDegreeOfParallelism = degree;
+        }
+
+        Parallel.For(
+            0,
+            numberOfChunks,
+            options,
+            chunkIndex =>
+            {
+                var start = chunkIndex * chunkSize;
+                var chunkLength = Math.Min(chunkSize, length - start);
+                var inputSpan = inputs.AsSpan(start, chunkLength);
+                var outputSpan = outputs.AsSpan(start, chunkLength);
+
+                PrimePhaseNative.GetInterferenceEnergyBatch(inputSpan, outputSpan);
+            });
+
+        return outputs;
     }
 }
